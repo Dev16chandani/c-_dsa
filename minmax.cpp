01743431cf1628830085bacdd15a89fc06280573
Dev16chandani <112163245+Dev16chandani@users.noreply.github.com>
2024-05-08 23:32:58 +0530

    Update minmax.cpp

--- minmax.cpp
+++ minmax.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     int arr[] = {1, 2, 3, 4, 5, 10, -1, 7};
-    auto [min,max] = std::minmax_element(begin(arr), end(arr));
+    auto [min,max]=minmax_element(begin(arr), end(arr));
     cout<<"Minimum: "<<*min<<endl;  // Output: Minimum: -1
     cout<<"Maximum: "<<*max<<endl;  // Output: Maximum: 10
     return 0;
